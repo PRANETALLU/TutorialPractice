73328d5adfe1e81524a06030382390596405f323
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2023-05-05 18:29:39 -0400

    new changes

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // npm init is used for editing package dependencies
 //gitignore is meant for files that are not being pushed to GitHub
+// npm run dev; you do not need to type the filename everytime or typing nodeman app.js
 
 const _ = require('lodash');
 
 const items = [1, [2, [3, [4]]]];
 const newItems = _.flattenDeep(items);
 console.log(newItems);
+console.log('hello people');
